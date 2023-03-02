d5a831901392f40e38447fa6fdaba30e542cab59
madrid7200 <106935787+madrid7200@users.noreply.github.com>
2023-03-02 09:47:16 -0600

    Map Class Factory

---
+++ HDT6/src/MapFactory.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.LinkedHashMap;
+import java.util.ArrayList;
+
+public class MapFactory {
+	
+	public static <K,V> Map <K,V> createMap(int opcion,int Key, ArrayList<Product> Product ){
+		switch() {
+		return Map<key, V>;
+	}
+		
+	
+
+
+ ;;;
